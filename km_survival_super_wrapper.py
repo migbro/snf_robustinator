cf614bc10b3da26275f285ffd8d461910b2a82c6
migbro <miguel.a.brown@gmail.com>
2017-03-24 14:12:17 -0500

    Added absolute paths to source files so that cd won't affect analysis

--- km_survival_super_wrapper.py
+++ km_survival_super_wrapper.py
@@ -5,22 +5,23 @@ from job_manager import job_manager
 from km_survival_wrapper import *
 
 if len(sys.argv) < 5:
-    sys.stderr.write('Usage: ' + sys.argv[0] + ' {source cluster data}{km survival r script location}'
+    sys.stderr.write('Usage: ' + sys.argv[0] + ' {km survival r script location}'
                                                '{permutated sample list}{permutated cluster list}{SNF param table}\n')
     exit(1)
 km_survival_wrapper = '/home/ubuntu/TOOLS/snf_robustinator/km_survival_wrapper.py'
-source_clust = sys.argv[1]
-r_script = sys.argv[2]
-perm_slist = sys.argv[3]
-perm_clist = sys.argv[4]
-tbl = open(sys.argv[5])
+r_script = sys.argv[1]
 cwd = os.getcwd()
+perm_slist = cwd + '/' + sys.argv[2]
+perm_clist = cwd + '/' + sys.argv[3]
+tbl = open(sys.argv[4])
+
 head = next(tbl)
 job_list = []
 for line in tbl:
     params = line.rstrip('\n').split('\t')
     (K, alpha, T) = params[1:3]
     wd = '_'.join((K, alpha, T))
+    source_clust = cwd + '/' + wd + '.txt'
     cmd = 'cd ' + cwd + '; mkdir ' + wd + ';cd ' + wd + ';'
     cmd += km_survival_wrapper + ' '.join((source_clust, r_script, perm_slist, perm_clist, K, alpha, T)) + ';'
     job_list.append(cmd)
