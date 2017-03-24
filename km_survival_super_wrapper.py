16083e958c2cf70c5ccf5043565318a39105f143
migbro <miguel.a.brown@gmail.com>
2017-03-24 16:58:59 -0500

    Fixed (hopefully) survival super wrapper to work with scrambler wrapper results

--- km_survival_super_wrapper.py
+++ km_survival_super_wrapper.py
@@ -5,16 +5,14 @@ import os
 from job_manager import job_manager
 
 
-if len(sys.argv) < 5:
-    sys.stderr.write('Usage: ' + sys.argv[0] + ' {km survival r script location}'
-                                               '{permutated sample list}{permutated cluster list}{SNF param table}\n')
+if len(sys.argv) < 3:
+    sys.stderr.write('Usage: ' + sys.argv[0] + ' {km survival r script location} {SNF param table}\n')
     exit(1)
 km_survival_wrapper = '/home/ubuntu/TOOLS/snf_robustinator/km_survival_wrapper.py'
 r_script = sys.argv[1]
 cwd = os.getcwd()
-perm_slist = cwd + '/' + sys.argv[2]
-perm_clist = cwd + '/' + sys.argv[3]
-tbl = open(sys.argv[4])
+
+tbl = open(sys.argv[2])
 
 head = next(tbl)
 job_list = []
@@ -23,7 +21,9 @@ for line in tbl:
     (K, alpha, T) = params[1:3]
     wd = '_'.join((K, alpha, T))
     source_clust = cwd + '/' + wd + '.txt'
-    cmd = 'cd ' + cwd + '; mkdir ' + wd + ';cd ' + wd + ';'
-    cmd += km_survival_wrapper + ' ' + ' '.join((source_clust, r_script, perm_slist, perm_clist, K, alpha, T)) + ';'
+    perm_slist = cwd + '/permuted_samples_1000x.txt'
+    perm_clist = cwd + '/permutation_res.txt'
+    cmd = 'cd ' + wd + ';'
+    cmd += km_survival_wrapper + ' ' + ' '.join((source_clust, r_script, perm_slist, perm_clist)) + ';'
     job_list.append(cmd)
 job_manager(job_list, '8')
