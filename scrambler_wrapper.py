a664eef44ebc22c3be6fc28610493d3edb895051
Ubuntu <ubuntu@mb-massive-vc-repeat.novalocal>
2017-03-24 14:45:54 -0500

    Debugged scrambler

--- scrambler_wrapper.py
+++ scrambler_wrapper.py
@@ -9,21 +9,22 @@ if len(sys.argv) < 5:
     sys.stderr.write('Usage: ' + sys.argv[0] + ' {r script }{table to permute} {iterations} {table to not permute} '
                                                '{SNF param table}\n')
     exit(1)
+scramble = '/home/ubuntu/TOOLS/snf_robustinator/scrambler.py'
 r_script = sys.argv[1]
 cwd = os.getcwd()
 eset2 = cwd + '/' + sys.argv[2]
-k = cwd + '/' + sys.argv[3]
-eset1 = sys.argv[4]
+k = sys.argv[3]
+eset1 = cwd + '/' + sys.argv[4]
 tbl = open(sys.argv[5])
 
 head = next(tbl)
 job_list = []
 for line in tbl:
     params = line.rstrip('\n').split('\t')
-    (K, alpha, T) = params[1:3]
+    (K, alpha, T) = params[0:3]
     wd = '_'.join((K, alpha, T))
 
     cmd = 'cd ' + cwd + '; mkdir ' + wd + ';cd ' + wd + ';'
-    cmd += r_script + ' ' + ' '.join((r_script, eset2, k, eset1, K, alpha, T)) + ';'
+    cmd += scramble + ' ' + ' '.join((eset2, k, eset1, r_script, K, alpha, T)) + ';'
     job_list.append(cmd)
 job_manager(job_list, '8')
